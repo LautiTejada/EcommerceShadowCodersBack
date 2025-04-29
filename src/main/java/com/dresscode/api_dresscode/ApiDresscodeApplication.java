6309fd7ba83d901d57e0016abecfb1532421def6
Marcos <marcosdimecom@gmail.com>
2025-04-29 12:40:40 -0300

    Proyecto agregado

---
+++ src/main/java/com/dresscode/api_dresscode/ApiDresscodeApplication.java
@@ -0,0 +1,13 @@
+package com.dresscode.api_dresscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDresscodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDresscodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dresscode/api_dresscode/ApiDresscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dresscode.api_dresscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDresscodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
