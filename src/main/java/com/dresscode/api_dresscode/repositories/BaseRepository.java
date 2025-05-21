022744259c753c7dd55b3d417dd98dc810cf77c5
Lauti <lautaro3314@hotmail.com>
2025-05-21 17:53:33 -0300

    bases creadas

---
+++ src/main/java/com/dresscode/api_dresscode/repositories/BaseRepository.java
@@ -0,0 +1,11 @@
+package com.dresscode.api_dresscode.repositories;
+
+import com.dresscode.api_dresscode.entities.Base;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import java.io.Serializable;
+
+@NoRepositoryBean
+public interface BaseRepository<E extends Base, ID extends Serializable> extends JpaRepository {
+}
