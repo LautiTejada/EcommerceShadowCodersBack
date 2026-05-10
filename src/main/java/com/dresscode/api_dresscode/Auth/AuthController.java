d3b3d336128ddfbb848e206fbb2679fe3212921a
Lauti Tejada <lautaro3314@hotmail.com>
2026-05-10 12:56:10 -0300

    feat(auth): add auth controller and security configuration beans

--- src/main/java/com/dresscode/api_dresscode/Auth/AuthController.java
+++ src/main/java/com/dresscode/api_dresscode/Auth/AuthController.java
@@ -15,7 +15,7 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping(value = "login")
-    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request){
+    public ResponseEntity<AuthResponse> login(@jakarta.validation.Valid @RequestBody LoginRequest request){
         return ResponseEntity.ok(authService.login(request));
     }
 
